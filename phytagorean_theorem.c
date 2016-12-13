3d6d50aa427a85aef86e7b515c3e743c3fb86595
Ramos, Jericho I <jerichoiquinramos@yahoo.com>
2016-12-13 08:35:47 +0800

    Create phytagorean_theorem.c

---
+++ phytagorean_theorem.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+main(){
+	int x;
+	int y;
+	int z;
+	
+	printf("Enter value for a: ");
+	scanf("%d", &a);
+	printf("Enter value for b: ");
+	scanf("%d", &b);
+	printf("%d", c = sqrt((a*a)+(b*b)));
+}
