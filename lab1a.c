f98f207de15600810b0fd186e01bd74f76afacbc
Ramos, Jericho I <jerichoiquinramos@yahoo.com>
2016-12-13 09:04:32 +0800

    Create lab1a.c

---
+++ lab1a.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+main(){
+	int a;
+	int b;
+	int c;
+	int d;
+	
+	printf("Enter value for a: ");
+	scanf("%d", &a);
+	printf("Enter value for b: ");
+	scanf("%d", &b);
+	printf("Enter value for c: ");
+	scanf("%d", &c);
+	scanf("%d", d);
+	d = (a+b+c/3);
+}
