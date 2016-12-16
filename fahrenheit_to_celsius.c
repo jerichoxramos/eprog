0532dd6131a5f4377ac551a16a4ed3db13310666
Ramos, Jericho I <jerichoiquinramos@yahoo.com>
2016-12-16 20:59:57 +0800

    Create fahrenheit_to_celsius.c

---
+++ fahrenheit_to_celsius.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+
+	int F, C;
+  
+	printf ("Farenheit Value:");
+	scanf ("%d", &F);
+	C = (F-32)*5/9;
+	printf ("Celsius Value:");
+	scanf ("%d", &C);
+  
+}
