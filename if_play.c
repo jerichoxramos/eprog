15dc3921294d1a6b2809ad0adfcf14e3465d649a
Ramos, Jericho I <jerichoiquinramos@yahoo.com>
2016-12-20 07:47:33 +0800

    Create if_play.c

---
+++ if_play.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+	int grade;
+	
+	printf("Enter your grade: ");
+	scanf("%d", &grade);
+	if(grade%3 == 0){
+		printf("Ed");
+	}
+	if(grade%5 == 0){
+		printf("Pogi");
+	}
+	if(grade%3,5 == 0){
+		printf("Ed pogi");
+	}
+}
