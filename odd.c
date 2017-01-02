171f4c54b8c750fa35b871f24cf0fffffc63d973
Ramos, Jericho I <jerichoiquinramos@yahoo.com>
2017-01-03 07:49:19 +0800

    Create odd.c

---
+++ odd.c
@@ -0,0 +1,17 @@
+#include <stdio.h>  
+   
+int main() 
+{ 
+    int y;   
+   
+    for(y = 1; y <= 100; y++) 
+	{  
+       
+        if(y%2 == 1) 
+		      { 
+            
+            printf("%d", y);  
+          }  
+    }  
+   
+}
